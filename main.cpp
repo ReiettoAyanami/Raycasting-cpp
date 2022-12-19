7a40c0c03707fed2c97c3d8e0fe764362ac415c5
ReiettoAyanami <tyno10159@gmail.com>
2022-12-19 15:47:00 +0100

    Removed sus comments.

--- main.cpp
+++ main.cpp
@@ -9,9 +9,6 @@
 #include "src/LRay.hpp"
 #include "src/LRenderer.hpp"
 
-
-//void f();
-
 int main(void)
 {
     const int screenWidth = 800;
@@ -21,7 +18,7 @@ int main(void)
     const int height = screenHeight * 2;
     
     // Fixed bug in LRay: Wrong collisions
-    //f();
+    
     
     srand(0);
 
@@ -34,7 +31,7 @@ int main(void)
     L::Ray obstacle(Vector2{100.f,100.f}, Vector2{300.f,300.f}, true, RED);
     L::Ray obstacle2(Vector2{200.f,100.f}, Vector2{400.f,300.f}, true, RED);
 
-    // fix shit in L::Renderer
+    // Disappearing ptr when passing a ptr;
 
     SetTargetFPS(60);              
 
@@ -67,38 +64,3 @@ int main(void)
 
     return 0;
 }
-
-// void f(){
-
-//     int a[10];
-//     int n;
-//     bool c = false;
-
-//     for(int i = 0; i < 10; ++i){
-//         do{
-//             std::cout << i << " - Inserire numero: " << std::endl;
-//             std::cin >> n;
-            
-
-            
-//             for(int j = 0; j < i; ++j){
-//                 if(n == a[j]){
-//                     c = true;
-//                     break;
-//                 }
-//                 else c = false;
-//             }
-//             if(c){
-//                 std::cout <<  " il numero " << n << " è già è presente" << std::endl;   
-//             }
-//         }while(c);
-//         a[i] = n;
-        
-
-        
-        
-
-//     }
-
-
-//}
